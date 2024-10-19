2685070d821ad8ad295eee9b643fdc2267dcd47a
Kamila <kamilaw397@gmail.com>
2024-10-19 18:50:42 +0200

    Initial commit

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,8 @@
 const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+console.log(randomNumber)
+if (randomNumber > 5) {
+    for (var i = 5; i < 11; i++) {    
+        console.log(i)}
+    } else {
+        console.log('Wylosowana liczba jest zbyt mala aby uzyc petli')
+    }
